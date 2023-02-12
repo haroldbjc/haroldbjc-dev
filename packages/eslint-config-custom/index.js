7745bccb953841cdfb8ed8660295cf446f716b94
Harold <harold.bjc@gmail.com>
2023-02-12 23:10:33 +0800

    Remove action and update package

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,13 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "turbo",
+    "prettier",
+  ],
+  plugins: ["prettier"],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    "prettier/prettier": "warn",
   },
 };
