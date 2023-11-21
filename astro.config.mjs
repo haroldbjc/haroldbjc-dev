7846c8037e3cba784638d1a8fdd6832183ced56d
Harold <harold.bjc@gmail.com>
2023-11-21 10:33:27 +0800

    Use mdx and update cache-control

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from '@astrojs/tailwind';
-
 import robotsTxt from "astro-robots-txt";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://haroldbjc.github.io',
+  output: 'static',
+  server: {
+    headers: {
+      "cache-control": "public, max-age=31536000"
+    }
+  },
   integrations: [react({
     include: ['**/react/*']
   }), tailwind({
     applyBaseStyles: false
-  }), robotsTxt()]
+  }), robotsTxt(), mdx()]
 });
