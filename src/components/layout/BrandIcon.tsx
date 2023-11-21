13da9ffff26b2d5bb4a4020caf9a049aaae6ea48
Harold <harold.bjc@gmail.com>
2023-11-21 17:21:55 +0800

    Update file paths and dependencies

---
+++ src/components/layout/BrandIcon.tsx
@@ -0,0 +1,44 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { BugIcon, ShellIcon } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast";
+
+const BrandIcon = () => {
+  const { toast } = useToast();
+  return (
+    <TooltipProvider delayDuration={2000}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <a
+            href="/"
+            className="font-italic mr-2 flex h-full flex-row items-center text-sm font-bold"
+          >
+            <ShellIcon className="mr-1 h-6 w-6" />
+            harold
+          </a>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>
+            <span className="mx-1 mb-1 inline-block align-middle">
+              <BugIcon
+                className="h-4 w-4"
+                onClick={() =>
+                  toast({
+                    description: "hi",
+                  })
+                }
+              />
+            </span>
+            perhaps you found a bug?
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default BrandIcon;
--- src/components/layout/ThemeToggle.tsx
+++ src/components/layout/ThemeToggle.tsx
@@ -28,7 +28,7 @@ export function ThemeToggle() {
   }, [theme]);
 
   return (
-    <DropdownMenu>
+    <DropdownMenu modal={false}>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
