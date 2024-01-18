868bc4b2039c2e936621fe610be90bdceccb85a3
Harold <harold.bjc@gmail.com>
2024-01-18 11:34:31 +0800

    🗒️ rebase file structure

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "haroldbjc.dev",
+  description:
+    "Personal website of Harold Bong, a technical lead and full-stack developer based in Malaysia.",
+  url: "https://haroldbjc.dev",
+  ogImage: "https:https://haroldbjc.dev/og.png",
+  links: {
+    twitter: "https://twitter.com/haroldbjc",
+    github: "https://github.com/haroldbjc",
+    linkedin: "https://www.linkedin.com/in/haroldbjc/",
+  },
+};
